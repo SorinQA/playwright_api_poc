bcd6ec98efaff436e869d29c869d358fd3037eae
Sorin Victor Dimofte <svd@intect.io>
2024-03-13 16:13:34 +0100

    Updated login test.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,28 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 let token;
 
 test.beforeAll(async ({ request }) => {
-    const response = await request.post('/api/auth/login');
-        expect(response.ok()).toBeTruthy(); 
-        const data = await response.json();
-        token = data.Token;
-        expect(data.Token).toBeTruthy();
-})
-  
-test.describe("Intect login module",()=>{
-    test('Get employment templates', async ({request}) => {   
-        const response = await request.get('/api/employmenttemplates/simple', {
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `Token ${token}`
-            }
-        });
-        
-        expect(response.ok()).toBeTruthy();
-        const data = await response.json();
-        expect(data[1].Name).toEqual('Fastansat');
-        // Passing assertion:
-        expect(data).toContainEqual(expect.objectContaining({"Id": 39708, "Name": "Fastansat"}));
-    })
-})
+  const response = await request.post("/api/auth/login");
+  expect(response.ok()).toBeTruthy();
+  const data = await response.json();
+  token = data.Token;
+  expect(data.Token).toBeTruthy();
+});
+
+test.describe("Intect login module", () => {
+  test("Get employment templates", async ({ request }) => {
+    const response = await request.get("/api/employmenttemplates/simple", {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Token ${token}`,
+      },
+    });
+
+    expect(response.ok()).toBeTruthy();
+    const data = await response.json();
+    expect(data[1].Name).toEqual("Fastansat");
+    expect(data).toContainEqual(
+      expect.objectContaining({ Id: 39708, Name: "Fastansat" })
+    );
+  });
+});
